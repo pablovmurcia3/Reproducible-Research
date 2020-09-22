d03aff9cb7637e0414890304a8c0cf03faf7af79
Pablo Velásquez <pablovmurcia@yahoo.com>
2020-09-21 19:02:23 -0500

    w1

---
+++ W1.R
@@ -0,0 +1,48 @@
+################################################################################
+                                     # Week 1 #
+################################################################################
+
+
+### Cocepts and ideas 
+
+# Replication: the ultimate standard for strengthening scientific evidence
+
+# Problem: Challenging -- Big studies, no money, really unique study
+
+
+# Solution: Reproducibility!!! is the minimun standard
+# Make the DATA and the CODE available 
+# Validation of the data analysis 
+
+
+#Justification: new technologies, computing power
+
+
+# What do we need?
+        
+# * Analytic data are available (Not the raw data)
+# * Analitic code are available (the code that is applied to the analytic data)
+# * Documentation od code and data
+# * Standard means of distribution 
+
+
+# Challenges ....
+
+# Tool: Literate (Statistical) Programming
+# Article is a steam of text and code 
+# It can be weaved to produce human-readable documents and tangled to produce
+# Machine-Readable documents
+
+# 1. A documentation Language (Human readable)
+# 2. A programming language (Machine-Readable)
+
+# Sweave uses latex and r!
+# but it have faults
+
+# Knir! better tan Sweave. 
+# It doesnt restrict to latex as the documentation language. It can use Markdown
+
+### Scripting your Analysis
+
+## Golden rule of reproducibility: Script everything
+# The score of the data analysis
