4106134ef83247e50a72afd469e2e3855be8e8c3
Pablo Velásquez <pablovmurcia@yahoo.com>
2021-01-22 17:07:21 -0500

    l

--- W1.R
+++ W1.R
@@ -47,39 +47,67 @@
 ## Golden rule of reproducibility: Script everything
 # The score of the data analysis
 
-#######################################################################
- # Estructure of a Data Analyzsis
-#######################################################################
+###### KNIR! better tan Sweave. ################################################
+
+# It doesnt restrict to latex as the documentation language. It can use Markdown
+
+### Scripting your Analysis ####################################################
+
+########### Golden rule of reproducibility: Script everything ##################
+
+# The score of the data analysis
+
+################################################################################
+###################### Structure of a Data Analysis ############################
+################################################################################
+
+
 # 1. Defining the question
+
+# defining a question is the most powerful dimension reduction tool you can ever
+# employ
+
+
 # 2. Defining the ideal dataset
+
 # 3. Determining what data you can access
+
 # 4. Obtaining the data
-# 5. Cleaning the data
-# 6.Exploratory data analysis
-# 7.Statistical prediction/modeling
-# 8.Interpretation of results
-# 9.Challenging of results
-# 10.Synthesis and write up
-# 11.Creating reproducible code
 
-
+# an example 
+install.packages("kernlab")
 library(kernlab)
 data(spam)
+
 str(spam)
+
+# 5. Cleaning the data
+
+
+# an example - in this case the data is already clean
+# Perform the subsampling - one to train the model and another to test it
 set.seed(3435)
-trainIndicator = rbinom(4601, size = 1, prob = 0.5)
+trainIndicator <- rbinom(4601, size = 1, prob = 0.5)
 table(trainIndicator)
 
-trainSpam = spam[trainIndicator == 1, ]
-testSpam = spam[trainIndicator == 0, ]
+trainSpam <- spam[trainIndicator == 1, ]
+testSpam <- spam[trainIndicator == 0, ]
+
+
+# 6. Exploratory data analysis
+
+names(trainSpam)
+head(trainSpam)
 
 table(trainSpam$type)
 
 boxplot(capitalAve ~ type, data = trainSpam)
+
 boxplot(log10(capitalAve + 1) ~ type, data = trainSpam)
+
 pairs(log10(trainSpam[, 1:4] + 1))
 
-hCluster = hclust(dist(t(trainSpam[, 1:57])))
+hCluster = hclust(dist((trainSpam[, 1:57])))
 plot(hCluster)
 
 hClusterUpdated = hclust(dist(t(log10(trainSpam[, 1:55] + 1))))
@@ -91,8 +119,6 @@ trainSpam$numType <- as.numeric(trainSpam$type) - 1
 costFunction <- function(x, y) sum(x != (y > 0.5))
 cvError = rep(NA, 55)
 library(boot)
-
-
 for (i in 1:55) { 
         lmFormula = reformulate(names(trainSpam)[i], response= "numType") # creates a fomula type numType ~ address
         glmFit = glm(lmFormula, family = "binomial", data = trainSpam)
@@ -104,6 +130,7 @@ names(trainSpam)[which.min(cvError)]
 
 lmFormula = reformulate(names(trainSpam)[53], response= "numType")
 glmFit = glm(lmFormula, family = "binomial", data = trainSpam)
+
 cvErrorpr = cv.glm(trainSpam, glmFit, costFunction, 2)$delta[2]
 
 
@@ -133,5 +160,53 @@ table(predictedSpam, testSpam$type)
 # Synthesizing Results
 
 # Reproducible Code
+
+############################################################################
+      # Organizing a Data Analysis #
+############################################################################
+
+## FILES
+# Data (Raw data and processed)
+# Figures (Exploratory, Final figures)
+# R code (raw, final. R markdown)
+# Text (README files. Text-Report)
+
+# 1. Raw Data
+
+# should be stored
+# PUt info (web, url, description) in the README
+# you can add the data en github
+
+# 2. Processed Data
+
+# should be named so it is ease to see which scrip generated the data
+# It should be tudy 
+
+# 3. Exploratory Figures
+
+# no pretty, useful
+
+# 4. Final Figure
+
+# 5. Raw scripts 
+
+# not necessary with comments
+
+# 6. Final Scripts
+
+# With comments, details
+
+# 7. R markdown files
+
+# summarize part of the analysis (text, and R)
+
+# 8. README
+
+# Not necessary if you use R markdown
+# should contain step-by-step of analysis
+
+# Text of the document
+
+
 
 
