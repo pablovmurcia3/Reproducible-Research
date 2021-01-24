385865be5a1666c3777a80926c0c4093a918dc9b
Pablo Velásquez <pablovmurcia@yahoo.com>
2021-01-24 18:47:10 -0500

    s

--- W2.R
+++ W2.R
@@ -2,3 +2,32 @@
                                 # Week 2 #
 ################################################################################
 
+# Coding Standards
+
+# 1. Always use text files / text editor
+
+# 2. Indent your code (space over to the right the flow structures)
+
+# 3. Limit the width of your code
+
+# 4. Limit the length of your functions 
+
+################################################################################
+ 
+# Markdown
+
+# The name “Markdown” is a play on the names of of many “markup”
+# languages
+
+# “Markdown is a text-to-HTML conversion tool
+# for web writers. Markdown allows you to write
+# using an easy-to-read, easy-to-write plain text
+# format, then convert it to structurally valid XHTML
+# (or HTML).
+
+
+
+
+
+
+
